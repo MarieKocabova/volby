cde298bf9860ceeeae9202916b140e44d794c426
MarieKocabova <maruspondeli@gmail.com>
2021-11-12 10:49:17 +0100

    hotovo

--- src/Candidate/index.jsx
+++ src/Candidate/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-const Candidate = (props) => (
-  <div className="candidate">
-    <h3 className="candidate__name">{props.name}</h3>
-    <img className="candidate__avatar" src={props.avatar} />
-    <button className="btn-vote">Vybrat</button>
-  </div>
-);
+const Candidate = ({ name, avatar, onVote }) => {
+  const handleVoteClick = () => {
+    onVote(name);
+  };
+
+  return (
+    <div className="candidate">
+      <h3 className="candidate__name">{name}</h3>
+      <img className="candidate__avatar" src={avatar} />
+      <button className="btn-vote" onClick={handleVoteClick}>
+        Vybrat
+      </button>
+    </div>
+  );
+};
 
 export default Candidate;
